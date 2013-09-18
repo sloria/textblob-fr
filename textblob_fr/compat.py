4a8e0b650a21cd269d556bfd0458870eeef3330a
Steven Loria <sloria1@gmail.com>
2013-09-18 09:26:31 -0500

    Trim down compat.py

--- textblob_fr/compat.py
+++ textblob_fr/compat.py
@@ -5,13 +5,7 @@ PY2 = int(sys.version[0]) == 2
 PY26 = PY2 and int(sys.version_info[1]) < 7
 
 if PY2:
-    def b(s):
-        return s
-    def u(s):
-        return unicode(s, "unicode_escape")
     from itertools import imap
-    import urllib2 as request
-    from urllib import quote as urlquote
     text_type = unicode
     binary_type = str
     string_types = (str, unicode)
@@ -19,12 +13,6 @@ if PY2:
     basestring = basestring
     imap = imap
 else:
-    def b(s):
-        return s.encode("latin-1")
-    def u(s):
-        return s
-    from urllib import request
-    from urllib.parse import quote as urlquote
     text_type = str
     binary_type = bytes
     string_types = (str,)
